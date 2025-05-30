c81d62a02e401e54c5cc9c7c90ee694888f5b251
GuiRL-Dev <guirl.dev@gmail.com>
2025-05-30 03:28:54 -0300

    Criando estrutura inicial

---
+++ src/app/components/default-login-layout/default-login-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-default-login-layout',
+  imports: [],
+  templateUrl: './default-login-layout.html',
+  styleUrl: './default-login-layout.scss'
+})
+export class DefaultLoginLayout {
+
+}
